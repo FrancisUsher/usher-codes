881a96ead54e4c1d77479607bccc507b8499afad
Francis Usher <francis.w.usher@gmail.com>
2019-10-11 17:59:36 -0700

    Implement some mobile app assets.

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            skipWaiting: true,
+        },
+        name: 'Usher Codes',
+        themeColor: '#303030',
+        msTileColor: '#424242',
+        appleMobileWebAppStatusBarStyle: 'black-translucent',
+    },
+};
