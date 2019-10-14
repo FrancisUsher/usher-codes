fced7c2989ea43925d814d36a0190d1b534654a4
Francis Usher <francis.w.usher@gmail.com>
2019-10-13 23:13:54 -0700

    Add list of job-cards to career page.

---
+++ src/pages/career/components/job-card.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-card>
+        <v-list-item two-line>
+            <v-list-item-content>
+                <v-list-item-title class="headline">{{
+                    title
+                }}</v-list-item-title>
+                <v-list-item-subtitle class="font-italic">
+                    {{ location }}
+                    <span class="float-right">{{ duration }}</span>
+                </v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+        <!-- <v-card-title class="headline"></v-card-title> -->
+        <!-- <v-card-subtitle></v-card-subtitle> -->
+        <v-card-text>
+            <ul>
+                <li
+                    v-for="(headline, i) in headlines"
+                    :key="i"
+                    v-html="headline"
+                ></li>
+            </ul>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-list-item class="grow">
+                <v-row align="center" justify="end">
+                    <v-btn disabled text>Details coming soon</v-btn>
+                </v-row>
+            </v-list-item>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'job-card',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: String,
+            required: true,
+        },
+        headlines: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
--- src/pages/career/index.vue
+++ src/pages/career/index.vue
@@ -1,10 +1,73 @@
 <template>
     <div>
         <h1 class="display-2">Career</h1>
+        <h2>I'm a web software engineer.</h2>
+        <v-container>
+            <v-row dense v-for="(job, i) in jobs" :key="i">
+                <v-col cols="12">
+                    <job-card
+                        :title="job.title"
+                        :location="job.location"
+                        :duration="job.duration"
+                        :headlines="job.headlines"
+                    ></job-card>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <v-btn disabled>PDF Résumé coming soon</v-btn>
     </div>
 </template>
 <script>
+import jobCard from './components/job-card';
 export default {
     name: 'career',
+    components: { jobCard },
+    data() {
+        return {
+            jobs: [
+                {
+                    title: 'Front-end engineer',
+                    location: 'Nexon OC, Irvine, CA',
+                    duration: '2018/01 - Present',
+                    headlines: [
+                        'Building a web gaming platform with <strong>Vue.js</strong>.',
+                        `Introduced <strong>test-driven</strong> front-end
+                                    development with Cypress.`,
+                        `Unified code style by integrating <strong>ESlint</strong> and
+                                    <strong>Prettier</strong>.`,
+                    ],
+                },
+                {
+                    title: 'Software engineer',
+                    location: 'Nexon America, El Segundo, CA',
+                    duration: '2016/08 - 2017/12',
+                    headlines: [
+                        `Built a web front &amp; back-end for gamer accounts site
+                            Scala Play framework and vanilla JS.`,
+                        `Integrated Twitch, Facebook, and other
+                                    third-party auth providers into Nexon Login.`,
+                        `Used Rollup and Uglify to write an
+                                    IE8-compatible Nexon auth library in under
+                                    4kb.`,
+                    ],
+                },
+                {
+                    title: 'Web developer',
+                    location: 'Nexon Europe, Berlin, Germany',
+                    duration: '2015/03 - 2016/08',
+                    headlines: [
+                        `Built lean, responsive, static pages with
+                                    Pug, Underscore, Bootstrap, and LESS.`,
+                        `Maintained a legacy enterprise C# ASP.net
+                                    2.0 web portal and platform system.`,
+                        `Patched vulnerabilities in new &amp; old
+                                    systems by applying modern security
+                                    best-practices.`,
+                    ],
+                },
+            ],
+        };
+    },
 };
 </script>
