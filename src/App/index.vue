0bff0c3ae43151aeb982967ad12222dd22ca0547
Francis Usher <francis.w.usher@gmail.com>
2019-12-08 22:28:31 -0800

    Add transitions to main router.

--- src/App/index.vue
+++ src/App/index.vue
@@ -3,7 +3,9 @@
         <app-bar></app-bar>
         <v-content>
             <v-container fluid>
-                <router-view></router-view>
+                <transition name="top-level" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </v-container>
         </v-content>
         <bottom-nav class="d-sm-none"></bottom-nav>
@@ -21,3 +23,20 @@ export default {
     }),
 };
 </script>
+<style lang="scss" scoped>
+.top-level-enter,
+.top-level-leave-to {
+    opacity: 0;
+}
+
+.top-level-leave-active {
+    transition: opacity 0.25s cubic-bezier(0, 0, 0.2, 1);
+}
+.top-level-enter-active {
+    transition: opacity 0.25s cubic-bezier(0.4, 0, 1, 1);
+}
+// .top-level-leave-active,
+// .top-level-enter-active {
+//     transition: opacity 2s cubic-bezier(0.4, 0, 0.2, 1);
+// }
+</style>
